fa27fb3061271f5c26c7296f02b085dba9aa9d64
jan <152862650+j-haacker@users.noreply.github.com>
2024-12-17 15:07:08 +0100

    chore: apply minor edits

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # snowMAUS
 
-Here, the snow cover model snowMAUS is being implemented in python.
+snowMAUS is a simple model that calculates the amounts of snowfall and meltwater
+production based on the precipitation and the minimum and maximum temperatures.
+The model was introduced in Agricultural and Forest Meteorology Issue 150, 2010,
+by M. Trnka and others: "Simple snow cover model for agrometeorological
+applications". This repository offers a python implementation of the model.
 
-snowMAUS was introduced in Agricultural and Forest Meteorology Issue 150, 2010, by M. Trnka and others: "Simple snow cover model for agrometeorological applications"
+
+## dependencies
+
+This implementation uses NumPy.
--- snowmaus.py
+++ snowmaus.py
@@ -6,8 +6,8 @@ __all__ = ["accumulation", "melt"]
 def accumulation(
         precipitation: np.typing.ArrayLike,
         temperature_daily_minimum: np.typing.ArrayLike,
-        threshold_temperature_upper: np.typing.ArrayLike = 0,
-        threshold_temperature_lower: np.typing.ArrayLike = -6
+        threshold_temperature_upper: float = 0,
+        threshold_temperature_lower: float = -6
 ) -> np.typing.ArrayLike:
     """Calculate snow accumulation
 
@@ -17,10 +17,10 @@ def accumulation(
     :type temperature_daily_minimum: np.typing.ArrayLike
     :param threshold_temperature_upper: Daily minimum temperature threshold above which snowfall
         is impossible, defaults to 0 ˚C
-    :type threshold_temperature_upper: np.typing.ArrayLike, optional
+    :type threshold_temperature_upper: float, optional
     :param threshold_temperature_lower: Daily minimum temperature threshold below which all
         precipitation is snowfall, defaults to -6 ˚C
-    :type threshold_temperature_lower: np.typing.ArrayLike, optional
+    :type threshold_temperature_lower: float, optional
     :return: Accumulated snow. Unit depends on input `precipitation`
     :rtype: np.typing.ArrayLike
     """
@@ -39,9 +39,9 @@ def accumulation(
 def melt(
         temperature_daily_minimum: np.typing.ArrayLike,
         temperature_daily_maximum: np.typing.ArrayLike,
-        threshold_temperature_minimum: np.typing.ArrayLike = -12,
-        threshold_temperature_maximum: np.typing.ArrayLike = 5,
-        melt_rate: np.typing.ArrayLike = 0.42  # mm/˚C/day
+        threshold_temperature_minimum: float = -12,
+        threshold_temperature_maximum: float = 5,
+        melt_rate: float = 0.42  # mm/˚C/day
 ) -> np.typing.ArrayLike:
     """Calculate meltwater runoff
 
@@ -51,13 +51,13 @@ def melt(
     :type temperature_daily_maximum: np.typing.ArrayLike
     :param threshold_temperature_minimum: Daily minimum temperature threshold below which melt is
         impossible, defaults to -12 ˚C
-    :type threshold_temperature_minimum: np.typing.ArrayLike, optional
+    :type threshold_temperature_minimum: float, optional
     :param threshold_temperature_maximum: Daily maximum temperature threshold below which melt is
         impossible, defaults to 5 ˚C
-    :type threshold_temperature_maximum: np.typing.ArrayLike, optional
+    :type threshold_temperature_maximum: float, optional
     :param melt_rate: Melt rate, defaults to 0.42 mm/˚C/day
-    :type melt_rate: np.typing.ArrayLike, optional
-    :return: Meltwater runoff
+    :type melt_rate: float, optional
+    :return: Meltwater runoff in mm
     :rtype: np.typing.ArrayLike
     """
     return np.where(
