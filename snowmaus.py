f512c806d4348261e62f0d56192fc1b9594dd7be
jan <152862650+j-haacker@users.noreply.github.com>
2024-12-17 13:05:20 +0100

    chore: vectorize accum. and melt functions

--- snowmaus.py
+++ snowmaus.py
@@ -4,68 +4,70 @@ __all__ = ["accumulation", "melt"]
 
 
 def accumulation(
-        precipitation: float,
-        temperature_daily_minimum: float,
-        threshold_temperature_upper: float = 0,
-        threshold_temperature_lower: float = -6
-) -> float:
+        precipitation: np.typing.ArrayLike,
+        temperature_daily_minimum: np.typing.ArrayLike,
+        threshold_temperature_upper: np.typing.ArrayLike = 0,
+        threshold_temperature_lower: np.typing.ArrayLike = -6
+) -> np.typing.ArrayLike:
     """Calculate snow accumulation
 
     :param precipitation: Precipitation
-    :type precipitation: float
+    :type precipitation: np.typing.ArrayLike
     :param temperature_daily_minimum: Daily minimum surface are temperature in degrees Celsius
-    :type temperature_daily_minimum: float
+    :type temperature_daily_minimum: np.typing.ArrayLike
     :param threshold_temperature_upper: Daily minimum temperature threshold above which snowfall
         is impossible, defaults to 0 ˚C
-    :type threshold_temperature_upper: float, optional
+    :type threshold_temperature_upper: np.typing.ArrayLike, optional
     :param threshold_temperature_lower: Daily minimum temperature threshold below which all
         precipitation is snowfall, defaults to -6 ˚C
-    :type threshold_temperature_lower: float, optional
+    :type threshold_temperature_lower: np.typing.ArrayLike, optional
     :return: Accumulated snow. Unit depends on input `precipitation`
-    :rtype: float
+    :rtype: np.typing.ArrayLike
     """
-    if temperature_daily_minimum <= threshold_temperature_lower:
-        return precipitation
-    elif temperature_daily_minimum < threshold_temperature_upper:
-        return (
+    return np.where(
+        temperature_daily_minimum <= threshold_temperature_lower,
+        precipitation,
+        np.where(
+            temperature_daily_minimum < threshold_temperature_upper,
             precipitation * (1 - (temperature_daily_minimum - threshold_temperature_lower)
-                             / np.abs(threshold_temperature_upper - threshold_temperature_lower))
+                             / np.abs(threshold_temperature_upper - threshold_temperature_lower)),
+            0
         )
-    return 0.
+    )
 
 
 def melt(
-        temperature_daily_minimum: float,
-        temperature_daily_maximum: float,
-        threshold_temperature_minimum: float = -12,
-        threshold_temperature_maximum: float = 5,
-        melt_rate: float = 0.42  # mm/˚C/day
-) -> float:
+        temperature_daily_minimum: np.typing.ArrayLike,
+        temperature_daily_maximum: np.typing.ArrayLike,
+        threshold_temperature_minimum: np.typing.ArrayLike = -12,
+        threshold_temperature_maximum: np.typing.ArrayLike = 5,
+        melt_rate: np.typing.ArrayLike = 0.42  # mm/˚C/day
+) -> np.typing.ArrayLike:
     """Calculate meltwater runoff
 
     :param temperature_daily_minimum: Daily minimum temperature in degrees Celsius
-    :type temperature_daily_minimum: float
+    :type temperature_daily_minimum: np.typing.ArrayLike
     :param temperature_daily_maximum: Daily maximum temperature in degrees Celsius
-    :type temperature_daily_maximum: float
+    :type temperature_daily_maximum: np.typing.ArrayLike
     :param threshold_temperature_minimum: Daily minimum temperature threshold below which melt is
         impossible, defaults to -12 ˚C
-    :type threshold_temperature_minimum: float, optional
+    :type threshold_temperature_minimum: np.typing.ArrayLike, optional
     :param threshold_temperature_maximum: Daily maximum temperature threshold below which melt is
         impossible, defaults to 5 ˚C
-    :type threshold_temperature_maximum: float, optional
+    :type threshold_temperature_maximum: np.typing.ArrayLike, optional
     :param melt_rate: Melt rate, defaults to 0.42 mm/˚C/day
-    :type melt_rate: float, optional
+    :type melt_rate: np.typing.ArrayLike, optional
     :return: Meltwater runoff
-    :rtype: float
+    :rtype: np.typing.ArrayLike
     """
-    # in the Trnka-2010 paper, it is not defined what should happen
-    # in case temperature_daily_minimum > 0 and
-    # temperature_daily_minimum < threshold_temperature_lower.
-    # this implementation assumes that, then, melt is possible.
-    if (
-        temperature_daily_minimum <= threshold_temperature_minimum
-        or (temperature_daily_minimum <= 0
-            and temperature_daily_maximum < threshold_temperature_maximum)
-    ):
-        return 0.
-    return melt_rate * (temperature_daily_minimum + np.abs(threshold_temperature_minimum))
+    return np.where(
+        np.logical_or(
+            temperature_daily_minimum <= threshold_temperature_minimum,
+            np.logical_and(
+                temperature_daily_minimum <= 0,
+                temperature_daily_maximum < threshold_temperature_maximum
+            )
+        ),
+        0,
+        melt_rate * (temperature_daily_minimum + np.abs(threshold_temperature_minimum))
+    )
